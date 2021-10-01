9331af2227f8efde2a256208b632908eca08a360
pedromps <90725927+pedromps@users.noreply.github.com>
2021-10-01 17:44:32 +0100

    Update unsuper.py
    implemented all methods for the balanced data.
    
    todo:
    add more metrics (like F1 score or AUC)
    implement the code with the same stuff but for the unbalanced data

--- unsuper.py
+++ unsuper.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.svm import SVC
-from sklearn.metrics import confusion_matrix, balanced_accuracy_score
-from sklearn.tree import DecisionTreeClassifier 
-from keras.models import Sequential
-from keras.layers import Dense
 from keras.callbacks import EarlyStopping
-from sklearn.model_selection import train_test_split
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.preprocessing import normalize
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, balanced_accuracy_score, accuracy_score
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn.linear_model import LogisticRegression
+
 
 x_train1 = np.load("dataset1_xtrain.npy")
 y_train1 = np.load("dataset1_ytrain.npy")
@@ -19,98 +21,61 @@ y_train2 = np.load("dataset2_ytrain.npy")
 x_test2 = np.load("dataset2_xtest.npy")
 y_test2 = np.load("dataset2_ytest.npy")
 
+x_train1 = normalize(x_train1, axis = 1)
+x_test1 = normalize(x_test1, axis = 1)
 
-print("MLP para o primeiro dataset:\n\n")
-model = Sequential()
-model.add(Dense(922, input_shape=(17,)))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(1, activation='softmax'))
-#model.summary()
+# There's no hints about the datasets. It is just known that it is a classification problem!
+# the first dataset is balanced while the second one is not
+# Neural Network (a simple MLP)
+ES = EarlyStopping(patience = 30, restore_best_weights=True)
+nn_clf = Sequential()
+nn_clf.add(Dense(128, activation = 'relu', input_shape = (x_train1.shape[1], )))
+nn_clf.add(Dense(32, activation = 'relu'))
+nn_clf.add(Dense(1, activation = 'sigmoid'))
+nn_clf.compile(optimizer = "Adam", loss = "binary_crossentropy", metrics = ["acc"])
+nn_clf.summary()
+history = nn_clf.fit(x_train1, y_train1, batch_size = 128, epochs = 500, validation_split = 0.2, callbacks = ES, verbose = 0)
 
-x_train, x_validation, y_train, y_validation = train_test_split(x_train1, y_train1, test_size=0.30)
-es = EarlyStopping(patience = 15, restore_best_weights=True)
-model.compile(optimizer='Adam',loss='mean_squared_error', metrics=['accuracy'])
-
-history_es1 = model.fit(x=x_train, y=y_train, epochs=200, batch_size=921, validation_data=(x_validation, y_validation), callbacks=[es])
-plt.figure(1)
-plt.plot(history_es1.history['loss'])
-plt.title('Perdas no modelo com Early Stopping')
-plt.ylabel('Perdas')
-plt.xlabel('Época')
-plt.legend('Treino', loc='upper right')
+plt.figure()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.grid()
 plt.show()
-print("\nConfusion Matrix = \n", confusion_matrix(y_test1, model.predict(x_test1)))
-
-print("MLP para o segundo dataset:\n\n")
-model = Sequential()
-model.add(Dense(231, input_shape=(10,)))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(1, activation='softmax'))
-#model.summary()
-
-x_train, x_validation, y_train, y_validation = train_test_split(x_train2, y_train2, test_size=0.30)
-es = EarlyStopping(patience = 15, restore_best_weights=True)
-model.compile(optimizer='Adam',loss='mean_squared_error', metrics=['accuracy'])
-
-history_es2 = model.fit(x=x_train, y=y_train, epochs=200, batch_size=231, validation_data=(x_validation, y_validation), callbacks=[es])
-plt.figure(1)
-plt.plot(history_es1.history['loss'])
-plt.title('Perdas no modelo com Early Stopping')
-plt.ylabel('Perdas')
-plt.xlabel('Época')
-plt.legend('Treino', loc='upper right')
-plt.show()
-print("\nConfusion Matrix = \n", confusion_matrix(y_test2, model.predict(x_test2)))
 
 
+print("\n\n\n\nLogistic Regression:")
+print("Training Accuracy of the model = {:.2f}".format(accuracy_score(y_train1, nn_clf.predict_classes(x_train1))))
+print("Confusion Matrix = \n", confusion_matrix(y_train1, nn_clf.predict_classes(x_train1)))
+print("Testing Accuracy of the model = {:.2f}".format(accuracy_score(y_test1, nn_clf.predict_classes(x_test1))))
+print("Confusion Matrix = \n", confusion_matrix(y_test1, nn_clf.predict_classes(x_test1)))
 
+# SVM with linear kernel and a harder margin formulation (best performance so far)
+svm_clf = SVC(kernel = 'linear', C = 100, gamma = 'auto')
+svm_clf.fit(x_train1, y_train1.ravel())
+print("\nSVM:")
+print("Training Accuracy of the model = {:.2f}".format(svm_clf.score(x_train1, y_train1)))
+print("Confusion Matrix = \n", confusion_matrix(y_train1, svm_clf.predict(x_train1)))
+print("Testing Accuracy of the model = {:.2f}".format(svm_clf.score(x_test1, y_test1)))
+print("Confusion Matrix = \n", confusion_matrix(y_test1, svm_clf.predict(x_test1)))
 
+# Decision tree, with pruning to try and generalise best (no node can have less than 10 instances)
+tree_clf = DecisionTreeClassifier(random_state = 0, min_samples_leaf = 10)
+tree_clf.fit(x_train1, y_train1)
+print("\n\n\n\nDecision Tree:")
+print("Training Accuracy of the model = {:.2f}".format(tree_clf.score(x_train1, y_train1)))
+print("Confusion Matrix = \n", confusion_matrix(y_train1, tree_clf.predict(x_train1)))
+print("Testing Accuracy of the model = {:.2f}".format(tree_clf.score(x_test1, y_test1)))
+print("Confusion Matrix = \n", confusion_matrix(y_test1, tree_clf.predict(x_test1)))
 
-#Testado com kernel rbf (Gaussiano)
-print("SVM com Kernel Gaussiano para o primeiro dataset:\n\n")
-clf = SVC(kernel = 'rbf', max_iter = 100000, gamma = 'auto')
-clf.fit(x_train1, y_train1.ravel())
-
-print("\nAccuracy do modelo treinado = ", clf.score(x_train1, y_train1))
-print("\nConfusion Matrix = ", confusion_matrix(y_train1, clf.predict(x_train1)))
-
-print("\nAccuracy do modelo com dataset de teste = ", clf.score(x_test1, y_test1))
-print("\nConfusion Matrix = \n", confusion_matrix(y_test1, clf.predict(x_test1)))
-
-
-print("SVM com Kernel Gaussiano para o segundo dataset:\n\n")
-clf = SVC(kernel = 'rbf', max_iter = 100000, gamma = 'auto')
-clf.fit(x_train2, y_train2.ravel())
-
-print("\nAccuracy do modelo treinado = ", balanced_accuracy_score(y_train2, clf.predict(x_train2)))
-print("\nConfusion Matrix = ", confusion_matrix(y_train2, clf.predict(x_train2)))
-
-print("\nAccuracy do modelo com dataset de teste = ", balanced_accuracy_score(y_test2, clf.predict(x_test2)))
-print("\nConfusion Matrix = \n", confusion_matrix(y_test2, clf.predict(x_test2)))
+# Regressions
+reg_clf = LogisticRegression(penalty = 'l2', C = 10)
+reg_clf.fit(x_train1, y_train1.ravel())
+print("\n\n\n\nLogistic Regression:")
+print("Training Accuracy of the model = {:.2f}".format(reg_clf.score(x_train1, y_train1)))
+print("Confusion Matrix = \n", confusion_matrix(y_train1, reg_clf.predict(x_train1)))
+print("Testing Accuracy of the model = {:.2f}".format(reg_clf.score(x_test1, y_test1)))
+print("Confusion Matrix = \n", confusion_matrix(y_test1, reg_clf.predict(x_test1)))
 
 
 
-
-
-print("Decision Tree para o primeiro dataset:\n\n")
-clf = DecisionTreeClassifier()
-clf.fit(x_train1, y_train1)
-
-print("\nAccuracy do modelo treinado = ", clf.score(x_train1, y_train1))
-print("\nConfusion Matrix = \n", confusion_matrix(y_train1, clf.predict(x_train1)))
-
-print("\nAccuracy do modelo com dataset de teste = ", clf.score(x_test1, y_test1))
-print("\nConfusion Matrix = \n", confusion_matrix(y_test1, clf.predict(x_test1)))
-
-
-print("Decision Tree para o segundo dataset:\n\n")
-clf = DecisionTreeClassifier()
-clf.fit(x_train2, y_train2)
-
-print("\nAccuracy do modelo treinado = ", clf.score(x_train2, y_train2))
-print("\nConfusion Matrix = \n", confusion_matrix(y_train2, clf.predict(x_train2)))
-
-print("\nAccuracy do modelo com dataset de teste = ", balanced_accuracy_score(y_test2, clf.predict(x_test2)))
-print("\nConfusion Matrix = \n", confusion_matrix(y_test2, clf.predict(x_test2)))
+# now for the unbalanced dataset
